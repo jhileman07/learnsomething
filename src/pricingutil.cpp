ebcfb508d5f7d233726ad660d62e918c2f655561
jhileman07 <johnny.hileman@icloud.com>
2023-10-08 22:59:51 -0500

    Changing initializing values to do the correct syntax

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,8 +1,6 @@
 #include "pricingutil.h"
 
-PricingUtil::PricingUtil() {
-  float val = 0;
-};
+PricingUtil::PricingUtil() : val(0.0) {};
 
 float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
   this->val = (prevPrice * (0.9f + interest) * oleoConstant);
