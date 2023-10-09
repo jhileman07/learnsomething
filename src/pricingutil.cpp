40ad76b523b120a0ba03279b4dafb3c32372805d
jhileman07 <johnny.hileman@icloud.com>
2023-10-08 21:09:07 -0500

    Initializing val in .cpp not just in .h as requested by Chris

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,6 +1,8 @@
 #include "pricingutil.h"
 
-PricingUtil::PricingUtil() {};
+PricingUtil::PricingUtil() {
+  float val = 0;
+};
 
 float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
   this->val = (prevPrice * (0.9f + interest) * oleoConstant);
