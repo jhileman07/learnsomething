47a1bdaca8cb49830fa0be4f4679c0eeb7250598
jhileman07 <johnny.hileman@icloud.com>
2023-10-14 21:38:08 -0500

    Completed step 4

--- prime.py
+++ prime.py
@@ -1,7 +1,8 @@
 import timeit
+import math
 
 def disasterCode():
-    for i in range (2,2500):
+    for i in range (2,1000):
         uniquePrimes = []
         currentPrime = i
         for j in range (2,i):
@@ -31,10 +32,27 @@ def goodCode():
         if prime:
             listPrimes.append(current)
 
+def bestCode():
+    is_prime = [True] * (1000)
+    is_prime[0] = is_prime[1] = False
+    listPrimes=[2]
+
+    for p in range(3, int(math.sqrt(1000)) + 1, 2):
+        if is_prime[p]:
+            for multiple in range(p * p, 1000, 2*p):
+                is_prime[multiple] = False
+
+    for i in range(3,1000,2):
+        if is_prime[i]:
+            listPrimes.append(i)
+
+
+
+
 # Benchmark the code
 if __name__ == "__main__":
-    benchmark_code = "goodCode()"
-    setup_code = "from __main__ import goodCode"
+    benchmark_code = "bestCode()"
+    setup_code = "from __main__ import bestCode"
 
     # Measure the execution time of disasterCode function
     times = []
